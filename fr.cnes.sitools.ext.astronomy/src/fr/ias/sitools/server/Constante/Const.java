6e1c2ffa920edd8d0bcecbe85e568d8f3c90c10d
marc <marc@inf-marc>
2014-03-17 16:19:54 +0100

    Clean version of Astronomy-Extension

--- fr.cnes.sitools.ext.astronomy/src/fr/ias/sitools/server/Constante/Const.java
+++
@@ -1,9 +0,0 @@
-package fr.ias.sitools.server.Constante;
-
-import fr.cnes.sitools.server.Consts; 
-
-public class Const implements Consts {
-  public static String APP_HOST_DOMAIN = "Starter.PUBLIC_HOST_DOMAIN";
-  public static String APP_PATH = "Starter.ROOT_DIRECTORY";
-}
-  
